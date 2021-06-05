806f2e252e2e6164cc36555aee2936ac51535be5
Christy Campbell <christytc10@gmail.com>
2021-06-05 17:21:38 +0100

    schedule splash

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import { Table, Tbody, Td, Th, Thead, Tr } from "@chakra-ui/table";
 import { Tab, TabList, TabPanel, TabPanels, Tabs } from "@chakra-ui/tabs";
 import { Image, Text, Stack, Box, Avatar,  Flex } from "@chakra-ui/react"
 import { useRouter } from "next/router";
+import { useCounter } from "@chakra-ui/counter"
 const league_api = `${process.env.league_api}`
 
 export async function getServerSideProps(context) { 
@@ -14,21 +15,34 @@ export async function getServerSideProps(context) {
   return { props: { schedule: schedule_data } };
 }
 
-export default function Home({ schedule }) {
+export default function Home({ schedule, counter }) {
   const router = useRouter();
   
   return (
     <Container maxW="container.xl">
-      {Object.keys(schedule).map((schedule_id) => (
-        <Flex w='100%'>
-          {schedule[schedule_id].map((team) => (
-            <Box>
-              <Image src={team.avatar} borderRadius="full" boxSize="25px" objectFit="cover"/>
-              {team.team}
-            </Box>
-          ))}
-        </Flex>
+      <Heading as="h3" size="md">Kickoff: 10 Sept 2021, 01:20</Heading>
+      <Table variant="striped">
+        <Thead>
+          <Tr>
+            <Th></Th>
+            <Th></Th>
+            <Th></Th>
+            <Th></Th>
+            <Th></Th>
+          </Tr>
+        </Thead>
+          <Tbody>
+          {Object.keys(schedule).map((schedule_id) => (
+          <Tr w='100%'>
+            <Td><Image src={schedule[schedule_id][0].avatar} borderRadius="full" boxSize="25px" objectFit="cover"/></Td>
+            <Td>{schedule[schedule_id][0].team}</Td>
+            <Td>vs</Td>
+            <Td textAlign='right'>{schedule[schedule_id][1].team}</Td>
+            <Td><Image src={schedule[schedule_id][1].avatar} borderRadius="full" boxSize="25px" objectFit="cover"/></Td>
+          </Tr>
       ))}
+      </Tbody>
+      </Table>
     </Container>
   );
 }
