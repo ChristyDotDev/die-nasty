962a1870906bd49b61f1470362a3dfb569428180
Christy Campbell <christytc10@gmail.com>
2021-06-12 08:22:23 +0100

    weekly schedule

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import { Container, Heading } from "@chakra-ui/layout";
 import { Table, Tbody, Td, Th, Thead, Tr } from "@chakra-ui/table";
+import { Tab, TabList, TabPanel, TabPanels, Tabs } from "@chakra-ui/tabs";
 import { Image } from "@chakra-ui/react"
 import { useRouter } from "next/router";
 import Countdown from "../components/Countdown"
@@ -11,8 +12,9 @@ export async function getServerSideProps(context) {
   const schedule_data = await schedule_res.json();
   //TODO - show full schedule
   console.log(schedule_data);
+
   
-  return { props: { schedule: schedule_data.fixtures[1], league_start: league_start } };
+  return { props: { schedule: schedule_data, league_start: league_start } };
 }
 
 export default function Home({ schedule, league_start }) {
@@ -29,21 +31,34 @@ export default function Home({ schedule, league_start }) {
     <Container maxW="container.xl">
       <Heading as="h3" size="md">Kickoff: 10 Sept 2021, 01:20</Heading>
       <Countdown countdownTo={league_start}></Countdown>
-      <Table variant="striped">
-        <Thead>
-        </Thead>
-          <Tbody>
-          {Object.keys(schedule).map((schedule_id) => (
-          <Tr w='100%'>
-            <Td onClick={() => handleRosterClick(schedule[schedule_id][0].roster)}><Image src={schedule[schedule_id][0].avatar} borderRadius="full" boxSize="25px" objectFit="cover"/></Td>
-            <Td onClick={() => handleRosterClick(schedule[schedule_id][0].roster)}>{schedule[schedule_id][0].team}</Td>
-            <Td>vs</Td>
-            <Td onClick={() => handleRosterClick(schedule[schedule_id][1].roster)} textAlign='right'>{schedule[schedule_id][1].team}</Td>
-            <Td onClick={() => handleRosterClick(schedule[schedule_id][1].roster)}><Image src={schedule[schedule_id][1].avatar} borderRadius="full" boxSize="25px" objectFit="cover"/></Td>
-          </Tr>
-      ))}
-      </Tbody>
-      </Table>
+      <Tabs isFitted colorScheme="teal" defaultIndex={schedule.current_week > 0 ? schedule.current_week - 1 : 0}>
+        <TabList>
+          {Object.keys(schedule.fixtures).map((week) => (
+            <Tab size="xs">Week {week}</Tab>
+          ))}
+        </TabList>
+        <TabPanels>
+          {Object.keys(schedule.fixtures).map((week) => (
+            <TabPanel>
+              <Table variant="striped">
+                <Thead>
+                </Thead>
+                  <Tbody>
+                  {Object.keys(schedule.fixtures[week]).map((fixture_id) => (
+                  <Tr w='100%'>
+                    <Td onClick={() => handleRosterClick(schedule.fixtures[week][fixture_id][0].roster)}><Image src={schedule.fixtures[week][fixture_id][0].avatar} borderRadius="full" boxSize="25px" objectFit="cover"/></Td>
+                    <Td onClick={() => handleRosterClick(schedule.fixtures[week][fixture_id][0].roster)}>{schedule.fixtures[week][fixture_id][0].team}</Td>
+                    <Td>vs</Td>
+                    <Td onClick={() => handleRosterClick(schedule.fixtures[week][fixture_id][1].roster)} textAlign='right'>{schedule.fixtures[week][fixture_id][1].team}</Td>
+                    <Td onClick={() => handleRosterClick(schedule.fixtures[week][fixture_id][1].roster)}><Image src={schedule.fixtures[week][fixture_id][1].avatar} borderRadius="full" boxSize="25px" objectFit="cover"/></Td>
+                  </Tr>
+              ))}
+              </Tbody>
+              </Table>
+            </TabPanel>
+          ))}
+        </TabPanels>
+      </Tabs>
     </Container>
   );
 }
